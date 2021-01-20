6fc569c5d94008d991b48dd7453165522124607e
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-20 21:35:48 +0000

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,13 +1,9 @@
 class CreateCats < ActiveRecord::Migration
-  def up
-    create_table :cats do |t|
-      t.string :name
-      t.integer :age
-      t.string :breed
+  def change
+      create_table :cats do |t|
+        t.string :name
+        t.integer :age
+        t.string :breed
+      end
     end
-  end
-  
-  def down
-    drop_table :cats
-  end
 end
